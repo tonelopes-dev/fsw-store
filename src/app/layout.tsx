2217e7ee64243a660e76b7b0e7bbcbbf287b2bfa
Tone Lopes <tonelopes.dev@gmail.com>
2024-11-18 10:59:53 -0300

    style: ensure footer is always on bottom

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,11 +17,14 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body>
-        <AuthProvider>
-          <Header />
-          {children}
-          <Footer />
-        </AuthProvider>
+        <div className="flex h-full flex-col">
+          <AuthProvider>
+            <Header />
+            <div className="flex-1">{children}</div>
+
+            <Footer />
+          </AuthProvider>
+        </div>
       </body>
     </html>
   );
