7ce3ccdf34a5cd867cdd058777b419252b7e2844
Tone Lopes <tonelopes.dev@gmail.com>
2024-11-18 08:57:53 -0300

    feat: add second banner to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -14,7 +14,7 @@ export default async function Home() {
     take: 8,
   });
   return (
-    <div className="">
+    <div>
       <Image
         src="/banner-home-01.png"
         sizes="100wv"
@@ -28,8 +28,18 @@ export default async function Home() {
         <Categories />
       </div>
       <div className="mt-8">
+        <p className="mb-3 pl-5 font-bold uppercase">Ofertas</p>
         <ProductList products={deals} />
       </div>
+      <Image
+        src="/banner-home-02.png"
+        sizes="100wv"
+        width={0}
+        height={0}
+        alt="Até 55% de desconto em mouses"
+        className="h-auto w-full px-5"
+        priority={true} // {false} | {true}
+      />
     </div>
   );
 }
