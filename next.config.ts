3c01b6f9feff72ab0b55dc2f3410d84621e82ac1
Tone Lopes <tonelopes.dev@gmail.com>
2024-11-18 07:35:30 -0300

    core: add s3 domain to next config file

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,12 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
