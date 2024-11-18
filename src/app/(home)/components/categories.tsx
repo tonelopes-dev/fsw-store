5dc317b3abbf2bbbe25f63f1fb64c925b377efec
Tone Lopes <tonelopes.dev@gmail.com>
2024-11-18 19:35:27 -0300

    dev?: remove console log

--- src/app/(home)/components/categories.tsx
+++ src/app/(home)/components/categories.tsx
@@ -3,7 +3,6 @@ import { CategoryItem } from "./category-item";
 
 const Categories = async () => {
   const categories = await prismaClient.category.findMany({});
-  console.log(categories);
   return (
     <div className="grid grid-cols-2 gap-x-4 gap-y-2">
       {categories.map((category) => (
