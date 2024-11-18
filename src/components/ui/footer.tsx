a4c15f019be5b44de6d3533dcbf1e8d2591837ba
Tone Lopes <tonelopes.dev@gmail.com>
2024-11-18 09:55:54 -0300

    fix: ensure opacity text in footer

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,7 +1,9 @@
 const Footer = () => {
   return (
-    <div className="bg-accent px-8 py-4 text-[0.625rem] opacity-75">
-      © 2023 Copyright <span className="font-bold">FSW Store</span>
+    <div className="bg-accent px-8 py-4 text-[0.625rem]">
+      <p className="opacity-75">
+        © 2023 Copyright <span className="font-bold">FSW Store</span>
+      </p>
     </div>
   );
 };
