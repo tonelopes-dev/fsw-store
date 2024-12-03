5570e04be58774d696bc55f9c220034ddf7cc117
Tone Lopes <tonelopes.dev@gmail.com>
2024-12-03 13:49:13 -0300

    style: add new style footer and header

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,10 +1,10 @@
 const Footer = () => {
   return (
-    <div className="bg-accent px-8 py-4 text-[0.625rem]">
-      <p className="opacity-75">
-        © 2023 Copyright <span className="font-bold">FSW Store</span>
-      </p>
-    </div>
+    <footer className="bg-accent px-8 py-4 text-[0.625rem] opacity-75 lg:text-sm">
+      <div className="mx-auto lg:container">
+        © 2023 Copyright <span className="font-semibold">FSW Store</span>
+      </div>
+    </footer>
   );
 };
 
--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -20,8 +20,8 @@ import {
   SheetTrigger,
 } from "./sheet";
 import { signIn, signOut, useSession } from "next-auth/react";
-import { Avatar, AvatarImage } from "./avatar";
-import { AvatarFallback } from "@radix-ui/react-avatar";
+import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
+
 import { Separator } from "./separator";
 import Link from "next/link";
 
@@ -36,99 +36,109 @@ const Header = () => {
   };
 
   return (
-    <Card className="flex items-center justify-between p-[1.875rem]">
-      <Sheet>
-        <SheetTrigger asChild>
-          <Button size="icon" variant="outline">
-            <MenuIcon />
-          </Button>
-        </SheetTrigger>
-        <SheetContent side={"left"}>
-          <SheetHeader className="text-left text-lg font-semibold">
-            <SheetTitle>Menu</SheetTitle>
-          </SheetHeader>
-
-          {status === "authenticated" && data?.user && (
-            <div className="flex flex-col">
-              <div className="flex items-center gap-3 py-3">
-                <Avatar>
-                  <AvatarFallback>
-                    {data?.user?.name?.charAt(0).toUpperCase()}
-                  </AvatarFallback>
-                  {data.user.image && <AvatarImage src={data.user.image!} />}
-                </Avatar>
-                <div className="flex flex-col">
-                  <p className="font-semibold">{data?.user?.name}</p>
-                  <p className="text-xs opacity-75">Boas compras!</p>
-                </div>
-              </div>
-              <Separator />
-            </div>
-          )}
-
-          <div className="mt-4 flex flex-col space-y-3">
-            {status === "unauthenticated" && (
-              <Button
-                onClick={handleLoginClick}
-                variant="outline"
-                className="w-full justify-start gap-2"
-              >
-                <LogInIcon size={16} />
-                Fazer login
-              </Button>
-            )}
-
-            {status === "authenticated" && (
-              <Button
-                onClick={handleLogoutClick}
-                variant="outline"
-                className="w-full justify-start gap-2"
-              >
-                <LogOutIcon size={16} />
-                Fazer Logout
-              </Button>
-            )}
-            <SheetClose asChild>
-              <Link href={"/"}>
-                <Button
-                  variant="outline"
-                  className="w-full justify-start gap-2"
-                >
-                  <HomeIcon size={16} />
-                  Início
-                </Button>
-              </Link>
-            </SheetClose>
-
-            <Button variant="outline" className="w-full justify-start gap-2">
-              <PercentIcon size={16} />
-              Ofertas
+    <header>
+      <Card className="flex items-center justify-between p-[1.875rem]">
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button size="icon" variant="outline">
+              <MenuIcon />
             </Button>
-            <SheetClose asChild>
-              <Link href="/catalog">
+          </SheetTrigger>
+          <SheetContent side={"left"}>
+            <SheetHeader className="text-left text-lg font-semibold">
+              <SheetTitle>Menu</SheetTitle>
+            </SheetHeader>
+
+            {status === "authenticated" && data?.user && (
+              <div className="flex flex-col">
+                <div className="flex items-center gap-3 py-3">
+                  <Avatar>
+                    <AvatarFallback>
+                      {data?.user?.name?.charAt(0).toUpperCase()}
+                    </AvatarFallback>
+                    {data.user.image && <AvatarImage src={data.user.image!} />}
+                  </Avatar>
+                  <div className="flex flex-col">
+                    <p className="font-semibold">{data?.user?.name}</p>
+                    <p className="text-xs opacity-75">Boas compras!</p>
+                  </div>
+                </div>
+                <Separator />
+              </div>
+            )}
+
+            <div className="mt-4 flex flex-col space-y-3">
+              {status === "unauthenticated" && (
                 <Button
+                  onClick={handleLoginClick}
                   variant="outline"
                   className="w-full justify-start gap-2"
                 >
-                  <ListOrderedIcon size={16} />
-                  Catálago
+                  <LogInIcon size={16} />
+                  Fazer login
                 </Button>
-              </Link>
-            </SheetClose>
-          </div>
-        </SheetContent>
-      </Sheet>
+              )}
 
-      <Link href={"/"}>
-        <h1 className="text-lg font-semibold">
-          <span className="font-bold text-primary">FSW</span> Store
-        </h1>
-      </Link>
+              {status === "authenticated" && (
+                <Button
+                  onClick={handleLogoutClick}
+                  variant="outline"
+                  className="w-full justify-start gap-2"
+                >
+                  <LogOutIcon size={16} />
+                  Fazer Logout
+                </Button>
+              )}
+              <SheetClose asChild>
+                <Link href={"/"}>
+                  <Button
+                    variant="outline"
+                    className="w-full justify-start gap-2"
+                  >
+                    <HomeIcon size={16} />
+                    Início
+                  </Button>
+                </Link>
+              </SheetClose>
 
-      <Button size="icon" variant="outline">
-        <ShoppingCartIcon />
-      </Button>
-    </Card>
+              <Button variant="outline" className="w-full justify-start gap-2">
+                <PercentIcon size={16} />
+                Ofertas
+              </Button>
+              <SheetClose asChild>
+                <Link href="/catalog">
+                  <Button
+                    variant="outline"
+                    className="w-full justify-start gap-2"
+                  >
+                    <ListOrderedIcon size={16} />
+                    Catálago
+                  </Button>
+                </Link>
+              </SheetClose>
+            </div>
+          </SheetContent>
+        </Sheet>
+
+        <Link href={"/"}>
+          <h1 className="text-lg font-semibold">
+            <span className="font-bold text-primary">FSW</span> Store
+          </h1>
+        </Link>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button size="icon" variant="outline">
+              <ShoppingCartIcon />
+            </Button>
+          </SheetTrigger>
+          <SheetContent side={"right"}>
+            <SheetHeader className="text-left text-lg font-semibold">
+              <SheetTitle>Meus produtos</SheetTitle>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+      </Card>
+    </header>
   );
 };
 
