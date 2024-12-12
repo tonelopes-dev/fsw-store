854cb2005783a577c7cfeba9a93f5400ec556eb2
Tone Lopes <tonelopes.dev@gmail.com>
2024-12-12 09:59:55 -0300

    feat: add loading pages

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <span className="loader"></span>
+    </div>
+  );
+};
+
+export default Loading;
