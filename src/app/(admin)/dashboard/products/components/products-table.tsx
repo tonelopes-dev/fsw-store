ab2bd6f48e1440d8bc6feb1b7ae57def7fb7ee62
Tone Lopes <tonelopes.dev@gmail.com>
2024-12-18 15:18:50 -0300

    feat: add initial products component

---
+++ src/app/(admin)/dashboard/products/components/products-table.tsx
@@ -0,0 +1,45 @@
+import {
+  TableCaption,
+  TableHeader,
+  TableRow,
+  TableHead,
+  TableBody,
+  TableCell,
+  Table,
+} from "@/components/ui/table";
+import { ProductWithTotalPrice } from "@/helpers/product";
+
+interface ProductsTableProps {
+  products: ProductWithTotalPrice[];
+}
+const ProductsTable = ({ products }: ProductsTableProps) => {
+  return (
+    <div>
+      <Table>
+        <TableCaption>A list of your recent invoices.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Nome</TableHead>
+            <TableHead>Categoria</TableHead>
+            <TableHead>Preço total</TableHead>
+            <TableHead>Preço base</TableHead>
+            <TableHead>Vendidos</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {products.map((product) => (
+            <TableRow key={product.id}>
+              <TableCell>{product.name}</TableCell>
+              <TableCell>{product.slug}</TableCell>
+              <TableCell>R$ {product.totalPrice.toFixed(2)}</TableCell>
+              <TableCell>R$ {product.basePrice.toFixed(2)}</TableCell>
+              <TableCell>{product.quantity}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
+};
+
+export default ProductsTable;
--- src/app/(admin)/dashboard/products/page.tsx
+++ src/app/(admin)/dashboard/products/page.tsx
@@ -2,9 +2,15 @@ import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { prismaClient } from "@/lib/prisma";
 import { Package, PlusIcon } from "lucide-react";
+import ProductsTable from "./components/products-table";
+import { computeProductTotalPrice } from "@/helpers/product";
 
 const ProductsPage = async () => {
   const products = await prismaClient.product.findMany({});
+
+  const productWithTotalPrice = products.map((product) =>
+    computeProductTotalPrice(product),
+  );
   return (
     <div className="flex w-full flex-col gap-10 p-10">
       <Badge variant="heading">
@@ -19,6 +25,7 @@ const ProductsPage = async () => {
           <PlusIcon size={16} /> Adicionar Produto
         </Button>
       </div>
+      <ProductsTable products={productWithTotalPrice} />
     </div>
   );
 };
