e13ee2b82d54fe5b01653e161ec9b91c476d9e93
Tone Lopes <tonelopes.dev@gmail.com>
2024-12-18 15:01:30 -0300

    feat: fetch data quantity in page Dashboard product and add button to plus product

--- src/app/(admin)/dashboard/products/page.tsx
+++ src/app/(admin)/dashboard/products/page.tsx
@@ -1,5 +1,26 @@
-const ProductsPage = () => {
-  return <h1>produts</h1>;
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { prismaClient } from "@/lib/prisma";
+import { Package, PlusIcon } from "lucide-react";
+
+const ProductsPage = async () => {
+  const products = await prismaClient.product.findMany({});
+  return (
+    <div className="flex w-full flex-col gap-10 p-10">
+      <Badge variant="heading">
+        <Package size={18} className="mr-2" />
+        Produtos
+      </Badge>
+      <div className="flex justify-between">
+        <p className="text-lg font-bold">
+          Produtos encontrados: {products.length}
+        </p>
+        <Button>
+          <PlusIcon size={16} /> Adicionar Produto
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default ProductsPage;
