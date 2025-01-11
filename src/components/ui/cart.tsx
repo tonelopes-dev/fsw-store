2d2f073140c5a3c3c557954a3e5174ea9a1590cf
Tone Lopes <tonelopes.dev@gmail.com>
2025-01-11 16:36:31 -0300

    feat: add validation puschase button before the checkout

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ShoppingCartIcon } from "lucide-react";
+import { Loader2, ShoppingCartIcon } from "lucide-react";
 import { Badge } from "./badge";
 import { useContext } from "react";
 import { CartContext } from "@/providers/cart";
@@ -12,25 +12,44 @@ import { Button } from "./button";
 import createCheckout from "@/actions/checkout";
 import { loadStripe } from "@stripe/stripe-js";
 import { SheetTitle } from "./sheet";
-import { useSession } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import { createOrder } from "@/actions/order";
 
+import { useState } from "react";
 const Cart = () => {
   const { data } = useSession();
   const { products, subtotal, total, totalDiscount } = useContext(CartContext);
+  const [isLoading, setIsLoading] = useState(false); // Estado para o loading
+
   const handleFinishPurchase = async () => {
     if (!data?.user) {
-      return;
+      alert("Você precisa estar logado para finalizar a compra.");
+      signIn(); // Redireciona para a página de login
+      return; // Interrompe a execução da função se o usuário não estiver logado
     }
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const order = await createOrder(products, (data?.user as any).id);
 
-    const sessionId = await createCheckout(products, order.id);
+    // Define o estado de carregamento como verdadeiro
+    setIsLoading(true);
 
-    const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY!);
+    try {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const order = await createOrder(products, (data?.user as any).id);
 
-    if (stripe) {
-      await stripe.redirectToCheckout({ sessionId });
+      const sessionId = await createCheckout(products, order.id);
+
+      const stripe = await loadStripe(
+        process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY!,
+      );
+
+      if (stripe) {
+        await stripe.redirectToCheckout({ sessionId });
+      }
+    } catch (error) {
+      console.error("Erro ao finalizar a compra:", error);
+      // Aqui você pode exibir um alerta ou tratamento de erro adequado
+    } finally {
+      // Define o estado de carregamento como falso após a conclusão
+      setIsLoading(false);
     }
   };
 
@@ -102,12 +121,26 @@ const Cart = () => {
           </div>
 
           {/* Botão Finalizar Compra */}
-          <Button
-            className="mt-7 font-bold uppercase"
-            onClick={handleFinishPurchase}
-          >
-            Finalizar compra
-          </Button>
+          {isLoading ? (
+            <Button disabled>
+              Carregando...
+              <Loader2 className="animate-spin" />
+            </Button>
+          ) : data?.user ? (
+            <Button
+              className="mt-7 font-bold uppercase"
+              onClick={handleFinishPurchase}
+            >
+              Finalizar compra
+            </Button>
+          ) : (
+            <Button
+              className="mt-7 font-bold uppercase"
+              onClick={() => signIn()}
+            >
+              Finalizar compra
+            </Button>
+          )}
         </div>
       )}
     </div>
